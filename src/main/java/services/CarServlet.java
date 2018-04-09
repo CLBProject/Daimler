5f832827413c5f2b36fc165729b709ed21ff23b8
Carlos Nobre <ext.carlos.nobre@ingg.com>
2018-04-09 19:08:43 +0100

    Finishing Exercise

--- src/main/java/services/CarServlet.java
+++ src/main/java/services/CarServlet.java
@@ -2,16 +2,18 @@ package services;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import templates.Groovy;
-import templates.exceptions.MethodNotExistsOnClassException;
-import templates.exceptions.VariableNotFoundException;
 
 public class CarServlet extends HttpServlet {
 	/**
@@ -22,40 +24,85 @@ public class CarServlet extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
-		
+
 		// Set the response message's MIME type
 		response.setContentType("text/html;charset=UTF-8");
 		// Allocate a output writer to write the response message into the network socket
 		PrintWriter out = response.getWriter();
 
-		Scanner in = new Scanner(getServletContext().getResourceAsStream(request.getRequestURI()));
+		Document document = Jsoup.parse(getServletContext().getResourceAsStream(request.getRequestURI()),"UTF-8",request.getRequestURI());
 
-		boolean groovyScript = false;
-		
-		Groovy groovy = new Groovy(request);
-		
-		while(in.hasNext()) {
-			String line = in.nextLine();
-			
-			if(line.contains("type=\"server/groovy\">")) {
-				groovyScript = true;
-			}
-			
-			else if(line.contains("</script>")){
-				groovyScript = false;
-			}
-			
-			if(groovyScript) {
-				try {
-					groovy.processGroovyToJava(line);
-				} catch (ClassNotFoundException | VariableNotFoundException | MethodNotExistsOnClassException e) {
-					out.println("<h1>" + e.getMessage() + "</h1>");
+		Element resultGroovy = document.select("script").first();
+
+		if(resultGroovy.attr("type").equals("server/groovy")) {
+			Groovy groovy = new Groovy(request, resultGroovy.data());
+
+			for (Element element : document.body().select("*")) {
+
+				if(element.nodeName().equals("div") && element.hasAttr("data-loop-model")) {
+					//TODO
+				}
+				else {
+					
+					for(Attribute attr: element.attributes()) {
+						renderAttribute(attr, groovy);
+					}
+					
+					if(element.hasAttr("data-if") && !Boolean.parseBoolean(element.attr("data-if"))) {
+						element.text("");
+					}
+					else {
+						element.removeAttr("data-if");
+						renderElementBody(element,groovy);
+					}
 				}
 			}
 		}
 		
-		in.close();
+		System.out.println(document.html());
+		
+		out.println(document.html());
 		out.close();
 
+	}
+
+	private void renderElementBody(Element element, Groovy groovy) {
+		String elemText = element.ownText();
+
+		String[] splitByExp = elemText.split("\\$\\{");
+
+		if(!elemText.equals("")) {
+
+			element.text(splitByExp[0]);
+
+			for(int i=1;i<splitByExp.length;i++) {
+				String[] content = splitByExp[i].split("\\}");
+
+				if(content.length > 0) {
+					element.append(groovy.getExpValue(content[0]));
+				}
+			}
+		}
+		
+	}
+	
+	private void renderAttribute(Attribute attr, Groovy groovy) {
+		String elemText = attr.getValue();
+
+		String[] splitByExp = elemText.split("\\$\\{");
+
+		if(!elemText.equals("")) {
+			
+			attr.setValue(splitByExp[0]);
+
+			for(int i=1;i<splitByExp.length;i++) {
+				String[] content = splitByExp[i].split("\\}");
+
+				if(content.length > 0) {
+					attr.setValue(attr.getValue()+groovy.getExpValue(content[0]));
+				}
+			}
+		}
+		
 	}
 }
--- src/main/java/templates/Groovy.java
+++ src/main/java/templates/Groovy.java
@@ -1,86 +1,25 @@
 package templates;
 
-import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
-import templates.exceptions.MethodNotExistsOnClassException;
-import templates.exceptions.VariableNotFoundException;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
 
 public class Groovy {
 
-	Map<String,Class> classes;
-	Map<String,Object> localVariables;
-	Map<String,Object> stateVariables;
+	GroovyShell shell;
 
-	public Groovy(HttpServletRequest req) {
-		classes = new HashMap<String,Class>();
-		localVariables = new HashMap<String,Object>();
-		localVariables.put("request", req);
-		stateVariables = new HashMap<String,Object>();
+	public Groovy(HttpServletRequest req, String groovyScript) {
+		Binding binding = new Binding();
+		binding.setVariable("request", req);
+		shell = new GroovyShell(binding);
+		shell.evaluate(groovyScript);
 	}
 
-	public void processGroovyToJava(String line) throws VariableNotFoundException, ClassNotFoundException, MethodNotExistsOnClassException {
-		if(line.startsWith("import ")) {
-			try {
-				String[] classSplited = line.split(" ");
-				String className = classSplited[1].split("\\.")[classSplited[1].split("\\.").length-1];
-				classes.put(className,Class.forName(classSplited[1]));
-			} catch (ClassNotFoundException e) {
-				throw new ClassNotFoundException();
-			}
-		}
-		else if(line.startsWith("def ")) {
-			String[] lineSplited = line.split(" ");
-
-			String localVariable = lineSplited[1];
-			Object value = evaluateExpression(lineSplited[3]);
-
-			localVariables.put(localVariable, value);
-		}
-		else {
-			String[] lineSplited = line.split(" ");
-
-			String stateVariable = lineSplited[0];
-			Object value = evaluateExpression(lineSplited[2]);
-
-			stateVariables.put(stateVariable, value);
-		}
-	}
-
-	private Object evaluateExpression(String expression) throws VariableNotFoundException, MethodNotExistsOnClassException {
-		if(expression.contains("\\.")) {
-			String[] expSplited = expression.split("\\.");
-
-			Class classFind = classes.get(expSplited[0]);
-			Object localVariable = localVariables.get(expSplited[0]);
-			Object stateVariable = stateVariables.get(expSplited[0]);
-
-			String method = expSplited[1];
-
-			if(classFind != null) {
-				Method methodToBeCalled;
-				try {
-					methodToBeCalled = classFind.getMethod(method);
-					methodToBeCalled.invoke(classFind);
-				} catch (Exception e) {
-					throw new MethodNotExistsOnClassException(e.getMessage());
-				}
-
-			}
-			else if (localVariable != null) {
-				//call method
-			}
-			else if(stateVariable != null) {
-				//call method
-			}
-			else {
-				throw new VariableNotFoundException("Variable not Found!");
-			}
-		}
-
-		return expression;
+	public String getExpValue(String string) {
+		
+		String[] expSplited = string.split("\\.");
+		
+		return "" + shell.evaluate(expSplited[0]+".get"+expSplited[1].substring(0, 1).toUpperCase()+expSplited[1].substring(1)+"()");
 	}
 }
